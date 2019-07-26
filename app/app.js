dff0fadd24bb105de5263fa5628b6872815327d2
jitendraOnline <jittu7777@gmail.com>
2019-07-26 18:56:05 +0530

    little modules

--- app/app.js
+++ app/app.js
@@ -3,12 +3,25 @@
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
+  'myApp.searchUser',
+  'myApp.adminReports',
+  'myApp.internalUser',
+  'myApp.externalUser',
+  'myApp.manageUser',
+  
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  $routeProvider.otherwise({redirectTo: '/searchUser'});
+}])
+
+.controller('appController',['$scope', function($scope) {
+  $scope.home = true;
+  $scope.searchUser = false;
+  $scope.manageUser = false;
+  $scope.adminReport = false;
+  $scope.internalUser = false;
+  $scope.externalUser = false;
+ }]);
